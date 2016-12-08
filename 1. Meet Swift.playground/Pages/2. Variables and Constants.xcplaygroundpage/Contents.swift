016ab937ab55221b0b2f561008cf6089402ae2f2
dssp100 <dssp@freenet.de>
2016-12-08 07:39:09 -0400

    Meet Swift done

---
+++ 1. Meet Swift.playground/Pages/2. Variables and Constants.xcplaygroundpage/Contents.swift
@@ -0,0 +1,36 @@
+//: ## 2.Variables and Constants
+// Variables
+var playersCurrentlyOnline = 3
+var age = 29
+var weight = 135.4
+
+// Cosntants
+let PI = 3.141592654
+let stateOfBirth = "New York"
+let yearMilesDavidWasBorn = 1926
+
+// Fittness Tracking App (My solution)
+let sex = "Male"
+let Name = "Peter"
+var location = "New York"
+var distanceRunTodayInMiles = 3
+var weightInLbs = 180.5
+var numeberOfPushups = 50
+var excersidedToday = true
+
+/* Fittness Tracking App (Thinkful)
+let name = "Peter"
+let sex = "male"
+var location = "New York"
+var weight = 180.5
+var milesRunToday = 3
+var numberOfPushups = 50
+var didWorkOutToday = true
+*/
+
+var distanceRunTodayInMiles: Double = 3.4
+
+
+
+
+//: [Previous: 1.Hello World ](@previous)  |  page 2 of 2  |  [Next: 3.](@next)
