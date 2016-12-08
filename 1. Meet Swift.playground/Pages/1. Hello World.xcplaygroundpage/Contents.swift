7f016e2c1d7c3514d6d7df9b0e5e1b6c961fdd85
dssp100 <dssp@freenet.de>
2016-12-08 07:38:57 -0400

    Meet Swift done

---
+++ 1. Meet Swift.playground/Pages/1. Hello World.xcplaygroundpage/Contents.swift
@@ -0,0 +1,10 @@
+//: ## 1.Hello World
+
+
+import Foundation
+
+var str = "Hello World"
+print("Hello World")
+print(str)
+
+//: [Previous: Overview ](@previous)  |  page 1 of 2  |  [Next: 2. Variables and Constants](@next)
